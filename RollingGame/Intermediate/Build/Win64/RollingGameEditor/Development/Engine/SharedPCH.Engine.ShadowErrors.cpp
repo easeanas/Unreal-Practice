b02ade830ad4502b90b6fa9d88646dda262c2579
Anastasia <easeanas@ecs.vuw.ac.nz>
2021-07-25 18:22:36 +1200

    Remove irrelevant files
    Former-commit-id: 6b3ebcd6f17ab03aa1d25c142b0649d6d3be0116

--- RollingGame/Intermediate/Build/Win64/RollingGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30040
-#include "C:/Users/anaea/source/repos/Unreal-Practice/RollingGame/Intermediate/Build/Win64/RollingGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- RollingGame/Source/RollingGame/RollingGame.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "RollingGame.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RollingGame, "RollingGame" );
- 
